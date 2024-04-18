a4c8aa346cdc1fe3d69edc7fab89e379599a3821
naglismoc <dvd.filmas@gmail.com>
2024-04-18 16:39:56 +0300

    break continue

--- main.py
+++ main.py
@@ -12,9 +12,54 @@ print("po visko")
 for i in numbersSequence:
     print(i)
 
-# [100, 101, 102, 4, 5, 6, 7, 8, 9]
 
-fruits = ["apple", "banana", "cherry"]
+#           0           1       2
+fruits = ["apple", "banana", "cherry","pineapple","plum","kiwi"]
+print(fruits)
 for fruit in fruits:
   print(fruit.upper())
+print("----------------------")
+skaiciai = [100, 101, 102, 4, 5, 6, 7, 8, 9]
+for number in skaiciai:
+    if number % 2 == 0:
+        print(number)
+print("po ciklo")
 
+print("----------------------")
+for number in skaiciai:
+    if number < 100:
+        break
+    print(number)
+print(";)")
+
+print("----------------------")
+for number in skaiciai:
+    if number % 2 == 0:
+        continue
+    print(number)
+print(";)")
+print("----------------------")
+fruits = ["apple", "banana", "cherry","pineapple","plum","kiwi"]
+
+for fruit in fruits:
+    if fruit == "pineapple":
+        continue
+    print(fruit)
+print("----------------------")
+
+for fruit in fruits:
+    if fruit == "banana":
+        break
+    print(fruit)
+print("----------------------")
+input = input("select a fruit:")
+hasFruit = False
+for fruit in fruits:
+    if fruit == input:
+        print("there you are, one",input)
+        hasFruit = True
+        break
+
+
+if not hasFruit:
+    print("sorry, we dont have such fruit")
